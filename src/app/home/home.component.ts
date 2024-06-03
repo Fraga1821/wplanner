1dd1e04c0023520d9887c71c49107af959888781
Lucas <lucas_r_cmrj@hotmail.com>
2024-06-03 19:22:37 -0300

    Changed material theme

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import{MatCardModule} from '@angular/material/card'
 
 @Component({
   selector: 'wp-home',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
